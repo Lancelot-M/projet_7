b72570557c30c31d528cef754329bb9005d61399
Lancelot Mitonneau <lancelotmitonneau@gmail.com>
2020-10-31 16:20:23 +0100

    git rm
    txt

---
+++ config_exemple.py
@@ -0,0 +1,3 @@
+"""Exemple of config.py file you must have. Change key with yours."""
+
+YOUR_GOOGLE_KEY = "dnf_'KZd9kdz8°0883fedcdD"
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+"""Flask launching file."""
+
 from GrandPyBot import app
 
 if __name__ == "__main__":
